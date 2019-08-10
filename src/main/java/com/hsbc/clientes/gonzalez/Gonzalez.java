d0de3b8d3f4413877f7b7b3c1b157097959050c4
ssdgg <lalitogbmex@hotmail.com>
2019-08-10 16:20:39 -0500

    upate

--- src/main/java/com/hsbc/clientes/gonzalez/Gonzalez.java
+++ src/main/java/com/hsbc/clientes/gonzalez/Gonzalez.java
@@ -1,5 +1,53 @@
 package com.hsbc.clientes.gonzalez;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hsbc.clientes.eduardocastillo.model.Cliente;
+
+
+
+@RestController
+@RequestMapping("/api/v1/Gonzalez/cliente")
 public class Gonzalez {
+static List<Cliente> clientes= new ArrayList<>();
+static {
+	Cliente juan = new Cliente();
+	juan.setId((long) 7l);
+	juan.setNombre("Juan");
+	juan.setApellidos("Gonzalez");
+	juan.setDireccion("Atlacomulco");
+	juan.setEdad(27);
+	juan.setGenero("Masculino");
+	clientes.add(juan);
+	
+}
+@PostMapping
+public Cliente add(@RequestBody Cliente cliente) {
+	cliente.setId(clientes.size() + 7L);
+	clientes.add(cliente);
+	return cliente;
+}
+
+@GetMapping
+public List<Cliente> findAll() {
+	return clientes;
+}
+
+@DeleteMapping("/{id}")
+public void deleteById(@PathVariable("id") Long id) {
+	clientes.remove(clientes.stream().filter(it -> it.getId().equals(id)).findFirst().get());
+}
 
 }
---
+++ src/main/java/com/hsbc/clientes/gonzalez/model/Cliente.java
@@ -0,0 +1,47 @@
+package com.hsbc.clientes.gonzalez.model;
+
+public class Cliente {
+private int id;
+private String nombre;
+private String apellidos;
+private int edad;
+private String genero;
+private String direccion;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellidos() {
+	return apellidos;
+}
+public void setApellidos(String apellidos) {
+	this.apellidos = apellidos;
+}
+public int getEdad() {
+	return edad;
+}
+public void setEdad(int edad) {
+	this.edad = edad;
+}
+public String getGenero() {
+	return genero;
+}
+public void setGenero(String genero) {
+	this.genero = genero;
+}
+public String getDireccion() {
+	return direccion;
+}
+public void setDireccion(String direccion) {
+	this.direccion = direccion;
+}
+
+}
