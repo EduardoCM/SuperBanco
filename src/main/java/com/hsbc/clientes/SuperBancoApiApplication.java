3337510ae1a2a1962d62022f9ac0f5d4f8dd42a3
Eduardo Castillo <ecastillo@anzen.com.mx>
2019-08-10 14:29:11 -0500

    first commit

---
+++ src/main/java/com/hsbc/clientes/SuperBancoApiApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperBancoApiApplication.class, args);
+	}
+
+}
