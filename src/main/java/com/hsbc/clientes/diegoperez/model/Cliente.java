55ad20ca033618ecba7d31a7b4ca65375fcdf51d
diego-prz04 <daprz0401@gmail.com>
2019-08-10 15:58:18 -0500

    aa

---
+++ src/main/java/com/hsbc/clientes/diegoperez/model/Cliente.java
@@ -0,0 +1,59 @@
+package com.hsbc.clientes.diegoperez.model;
+
+public class Cliente {
+	private Long id;
+	private String nombre;
+	private String apellidos;
+	private String direccion;
+	private int edad;
+	private String genero;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+}
